c7d58c667f8e75d1e1b5f6ef1118104b36d51eb9
Polinakhot <polinak9@nycstudents.net>
2025-05-06 22:01:35 +0000

    done

--- Main.java
+++ Main.java
@@ -1,1 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
 
+        Wordmatch game1 = new Wordmatch("mississippi");
+
+        System.out.println("Score for 'i': " + game1.scoreGuess("i"));                
+        System.out.println("Score for 'iss': " + game1.scoreGuess("iss"));             
+        System.out.println("Score for 'issip': " + game1.scoreGuess("issip"));        
+        System.out.println("Score for 'mississippi': " + game1.scoreGuess("mississippi")); 
+
+    
+        Wordmatch game2 = new Wordmatch("aaaabb");
+
+        System.out.println("Score for 'a': " + game2.scoreGuess("a"));                 
+        System.out.println("Score for 'aa': " + game2.scoreGuess("aa"));               
+        System.out.println("Score for 'aaa': " + game2.scoreGuess("aaa"));             
+        System.out.println("Score for 'aabb': " + game2.scoreGuess("aabb"));           
+        System.out.println("Score for 'c': " + game2.scoreGuess("c"));                
+
+        Wordmatch game3 = new Wordmatch("concatenation");
+
+        System.out.println("findBetterGuess('ten', 'nation'): " + game3.findBetterGuess("ten", "nation")); 
+        System.out.println("findBetterGuess('con', 'cat'): " + game3.findBetterGuess("con", "cat"));       
+    }
+}
---
+++ Wordmatch.java
@@ -0,0 +1,35 @@
+public class Wordmatch {
+    /** The secret string */
+    private String secret;
+
+    /** Constructs a WordMatch object with the given secret string */
+    public Wordmatch(String word) {
+        secret = word;
+    }
+
+    /** Returns a score for guess */
+    public int scoreGuess(String guess) {
+        int count = 0;
+        for (int i = 0; i <= secret.length() - guess.length(); i++) {
+            if (secret.substring(i, i + guess.length()).equals(guess)) {
+                count++;
+            }
+        }
+        return count * guess.length() * guess.length();
+    }
+
+    /** Returns the better of two guesses, using scoreGuess and tie-breaking rules */
+    public String findBetterGuess(String guess1, String guess2) {
+        int score1 = scoreGuess(guess1);
+        int score2 = scoreGuess(guess2);
+
+        if (score1 > score2) {
+            return guess1;
+        } else if (score2 > score1) {
+            return guess2;
+        } else {
+            // Tie-breaker: return the alphabetically later string
+            return (guess1.compareTo(guess2) > 0) ? guess1 : guess2;
+        }
+    }
+}
